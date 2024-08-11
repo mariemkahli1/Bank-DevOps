7873544518cbe6c4f8fc7bc67747ac7c35dc2869
mariemkahli1 <kahli.mariem@enis.tn>
2024-08-11 23:42:22 +0100

    Update footer.php

--- src/footer.php
+++ src/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer bg-light text-center py-4">
-     <p>&copy; 2024 Flare Bank. All rights reserved. Mariem Kahli</p>
+     <p>&copy; 2024 Flare Bank. All rights reserved.</p>
 </footer>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-HXXKkA9rla5rKT7lN5N7QwW239g6TA2QrVsZz3tc6PvFbF/9ZzU5gKSOkLKk6U7Q" crossorigin="anonymous"></script>
